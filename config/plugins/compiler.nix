cd95c3db3a4f87628c3e9899418b9d0e7a0aeb8e
Olisnot <oliverpm2007@gmail.com>
2024-10-22 21:36:27 +0100

    adding keymap for opening compiler menu

--- config/plugins/compiler.nix
+++ config/plugins/compiler.nix
@@ -2,6 +2,11 @@
   keymaps = [
     {
       action = "<cmd>CompilerToggleResults<CR>";
+      key = "<leader>ct";
+      mode = [ "n" ];
+    }
+    {
+      action = "<cmd>CompilerOpen<CR>";
       key = "<leader>c";
       mode = [ "n" ];
     }
