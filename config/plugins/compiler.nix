a6f2d0ab5371cb6a4cf5637fef22b97454c44e23
Olisnot <oliverpm2007@gmail.com>
2024-10-22 21:33:50 +0100

    adding keymap for toggling compiler results

--- config/plugins/compiler.nix
+++ config/plugins/compiler.nix
@@ -1,4 +1,11 @@
 {
+  keymaps = [
+    {
+      action = "<cmd>CompilerToggleResults<CR>";
+      key = "<leader>c";
+      mode = [ "n" ];
+    }
+  ];
   plugins = {
     compiler.enable = true;
     overseer.enable = true;
