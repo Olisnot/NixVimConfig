1ffa74cc244d522c4b2b4dcdab4dfb462067d4a5
Olisnot <oliverpm2007@gmail.com>
2024-10-30 15:19:52 +0000

    disabling some options

--- config/options.nix
+++ config/options.nix
@@ -3,9 +3,9 @@
     relativenumber = true;
     nu = true;
     conceallevel = 2;
-    hlsearch = false;
-    incsearch = true;
-    scrolloff = 16;
+    #hlsearch = false;
+    #incsearch = true;
+    #scrolloff = 16;
   };
   globals.mapleader = " ";
 }
